8a3d0698b97e75924d63d9c2c928c7df94149ab4
trivopia <135800895+trivopia@users.noreply.github.com>
2025-03-29 17:43:18 +0700

    modified:   main.js
    Created function for the innitiale rock-paper-scissor game.
    Includes if statements and console.logs

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 let humanScore = 0;
 let computerScore = 0;
+let drawScore = 0;
 
 function getComputerChoice() {
   let randNum = Math.floor(Math.random() * 3 + 1);
+
   if (randNum === 1) {
     return "rock";
   } 
@@ -15,6 +17,32 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let myChoice = prompt("Choose your bet!",)
-  return myChoice.toLowerCase;
+  let myChoice = prompt("Choose your bet",)
+  return myChoice.toLowerCase();
+}
+
+function playRound() {
+  let computerChose = getComputerChoice();
+  let youChose = getHumanChoice();
+
+  console.log("You chose " +  youChose + ". Computer chose " + computerChose)
+
+  if (computerChose === youChose) {
+    console.log("Draw!");
+    drawScore ++;
+  }
+  else if (
+    (youChose === "rock" && computerChose === "scissor") || 
+    (youChose === "paper" && computerChose === "rock") ||
+    (youChose === "scissor" && computerChose === "paper")
+  ) {
+    console.log("You Win!");
+    humanScore ++;
+  }
+  else {
+    console.log("You lose!");
+    computerScore ++;
+  }
+
+  console.log(`Scoreboard [Wins: ${humanScore} Loses: ${computerScore} Draws: ${drawScore}]`);
 }
